818462442f10a51ed77a107f32a4cf4221da305d
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-09-24 11:48:21 +0300

    fix: bug

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import HeroTitle from "./HeroTitle"
 const Hero = () => {
   const title = "Hi, my name is Hamit Can"
   return (
-    <main className='space-y-16 md:space-y-0 '>
+    <main className='space-y-16 md:space-y-0 place-self-center'>
       <HeroTitle text={title} />
 
       <section className='grid grid-cols-1 place-items-center gap-y-8 md:gap-y-0 gap-x-0 md:gap-x-20 lg:gap-x-0 md:grid-cols-2'>
@@ -13,7 +13,7 @@ const Hero = () => {
           do not hesitate to learn new technologies, encourage colleagues to
           become better together is pleasure to me.
         </p>
-        <div className='w-96 h-96'>
+        <div className='max-w-full w-96 h-96'>
           <HeroAnimation />
         </div>
       </section>
