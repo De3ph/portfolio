fb1955862bd8366f32359d549aed4cbefa45e583
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-10-05 11:57:01 +0300

    ui imp & animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,34 @@
+import { useEffect } from "react"
+import { animate, spring } from "motion"
+
 import HeroAnimation from "./HeroAnimation.tsx"
 import HeroTitle from "./HeroTitle"
 import content from "@content"
 
 const Hero = () => {
+  useEffect(() => {
+    const heroDesc = document.getElementById("heroDesc")
+    if (heroDesc) {
+      animate(
+        heroDesc,
+        {
+          opacity: [0.2, 0.8, 1],
+          scale: [0.8, 1]
+        },
+        {
+          easing: spring({
+            mass: 1.4,
+            from: 1.2,
+            velocity: 0.1,
+            stiffness: 80
+          }),
+          duration: 3
+        }
+      )
+    }
+  })
   return (
-    <main className='space-y-16 md:space-y-0 place-self-center'>
+    <main id='heroDesc' className='space-y-16 md:space-y-0 place-self-center'>
       <HeroTitle text={content.firstPage.hero.title} />
 
       <section className='grid grid-cols-1 place-items-center gap-y-8 md:gap-y-0 gap-x-0 md:gap-x-20 lg:gap-x-0 md:grid-cols-2'>
--- src/components/Hero/HeroTitle.tsx
+++ src/components/Hero/HeroTitle.tsx
@@ -6,28 +6,9 @@ type Props = {
 }
 
 const HeroTitle = (props: Props) => {
-  useEffect(() => {
-    const el = document.getElementById("el")
-    if (el) {
-      animate(
-        el,
-        {
-          opacity: [0.2, 0.8, 1],
-          scale: [0.8, 1]
-        },
-        {
-          easing: spring(),
-          duration: 3
-        }
-      )
-    }
-  })
   return (
     <div>
-      <p
-        id='el'
-        className='bg-clip-text bg-gradient-to-r from-indigo-300 to-indigo-600 text-transparent font-semibold text-3xl md:text-4xl'
-      >
+      <p className='bg-clip-text bg-gradient-to-r from-indigo-300 to-indigo-600 text-transparent font-semibold text-3xl md:text-4xl'>
         {props.text}
       </p>
     </div>
