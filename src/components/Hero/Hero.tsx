7b9bb0a1ae891f391459a4c1c7a9a8aa453d8f3a
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-10-07 22:33:12 +0300

    feat: add typewriter effect and subtitles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -32,10 +32,13 @@ const Hero = () => {
       id='heroDesc'
       className='space-y-16 md:space-y-0 place-self-center opacity-0'
     >
-      <HeroTitle text={content.firstPage.hero.title} />
+      <HeroTitle
+        title={content.firstPage.hero.title}
+        subtitles={content.firstPage.hero.subtitles}
+      />
 
       <section className='grid grid-cols-1 place-items-center gap-y-8 md:gap-y-0 gap-x-0 md:gap-x-20 lg:gap-x-0 md:grid-cols-2'>
-        <p className='font-normal text-lg xl:text-3xl leading-loose first-letter:text-3xl first-letter:font-semibold'>
+        <p className='font-normal text-lg xl:text-3xl leading-loose first-letter:text-3xl first-letter:font-semibold whitespace-pre-line'>
           {content.firstPage.hero.text}
         </p>
         <div className='max-w-full w-96 h-96'>
--- src/components/Hero/HeroTitle.tsx
+++ src/components/Hero/HeroTitle.tsx
@@ -1,11 +1,24 @@
+import { Typewriter } from "react-simple-typewriter"
+
 type Props = {
-  text: string
+  title: string
+  subtitles?: string[]
 }
 
 const HeroTitle = (props: Props) => {
   return (
     <div className='font-semibold text-3xl md:text-4xl bg-gradient-to-r bg-clip-text text-indigo-300'>
-      <p>{props.text}</p>
+      <p>{props.title}</p>
+      {props.subtitles && (
+        <Typewriter
+          words={props.subtitles}
+          cursor
+          loop
+          typeSpeed={100}
+          deleteSpeed={50}
+          delaySpeed={500}
+        />
+      )}
     </div>
   )
 }
