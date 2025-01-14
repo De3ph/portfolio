5fa275a3dcfc8d3866564d1129ccd8fbf37fc41d
Hamit Can <62203579+De3ph@users.noreply.github.com>
2025-01-14 23:58:52 +0300

    Update Card.tsx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -27,11 +27,11 @@ const Card = (props: Props) => {
       transition-all ease-out duration-300 
        border-[1px] border-stone-700 border-collapse lg:border-opacity-10 lg:hover:border-opacity-100'
       >
-        <div className='h-[7rem] rounded-md'>
+        <div className='h-[7rem]'>
           <img
             src={props?.image?.url}
             alt={props?.image?.alt}
-            className='h-full w-full object-cover lg:group-hover:opacity-60 transition-opacity duration-500'
+            className='h-full w-full rounded-md object-cover lg:group-hover:opacity-60 transition-opacity duration-500'
           />
         </div>
         <div className='p-3'>
